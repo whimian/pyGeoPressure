24bcc9b1420bc3f29a1c7f8fbb74b35748ef60da
Yu Hao <yuhao89@live.cn>
2016-06-24 15:33:51 +0800

    make Log class visible to the outside scope

--- porepressureprediction/__init__.py
+++ porepressureprediction/__init__.py
@@ -1,4 +1,5 @@
 from porepressureprediction.velocity.basic.laSQL import Well
+from porepressureprediction.velocity.basic.laSQL import Log
 from porepressureprediction.velocity.basic.seiSQL import SeisCube
 from porepressureprediction.velocity.basic.survey import Survey
 from porepressureprediction.velocity.reader import Reader
