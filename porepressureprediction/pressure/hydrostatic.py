4d1d4d52ad6a5fc208fe036cac84ffb465e7b2e7
Yu Hao <yuhao89@live.cn>
2016-12-18 17:00:05 +0800

    correct hydrostatic()

--- porepressureprediction/pressure/hydrostatic.py
+++ porepressureprediction/pressure/hydrostatic.py
@@ -4,19 +4,42 @@ A straightforward 2D kriging program
 
 Created on Fri Nov 11 2016
 """
-def hydrostatic_pressure(depth):
+import numpy as np
+
+
+def hydrostatic_pressure(depth, kelly_bushing=41, depth_w=82, rho_f=1.174, rho_w=1.01):
     """
     Parameters
     ----------
     depth : scalar or 1-d ndarray
-        unit: meter
+        measured depth, unit: meter
+    rho_f : scalar
+        density of pore fluid, g/cm3
+    kelly_bushing : scalar
+        kelly bushing elevation, in meter
+    depth_w : scalar
+        sea water depth
+    rho_w : scalar
+        sea water density
 
     Returns
     -------
     pressure : scalar or 1-d ndarray
         unit: mPa
     """
-    den_w = 1.174  # g/c3
-    den_w *= 1000  # kg/m3
+    rho_f *= 1000  # kg/m3
+    rho_w *= 1000  # kg/m3
     acceleration = 9.80665  # m/s2
-    return depth * acceleration * den_w / 10**6  # mPa
+    density = np.full_like(depth, rho_f)
+    # sea level to sea bottom
+    mask = depth < (kelly_bushing + depth_w)
+    density[mask] = rho_w
+    # kelly bushing to sea level
+    mask = depth < kelly_bushing
+    density[mask] = 0
+    delta_depth = np.full_like(depth, np.nan)
+    delta_depth[1:] = depth[1:] - depth[:-1]
+    delta_depth[0] = 0
+    hydrostatic = delta_depth * density * acceleration / 1000000  # mPa
+    hydrostatic = np.cumsum(hydrostatic)
+    return hydrostatic
