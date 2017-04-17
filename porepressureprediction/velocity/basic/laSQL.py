5a839c483527fe14f7b18e7f5a54aa2d8ed64cc8
Yu Hao <yuhao89@live.cn>
2017-04-17 10:56:33 +0800

    refine write_od() dealing with inf value

--- porepressureprediction/velocity/basic/laSQL.py
+++ porepressureprediction/velocity/basic/laSQL.py
@@ -461,13 +461,12 @@ class Log(object):
     def write_od(self, file_name):
         try:
             with open(file_name, 'w') as fout:
-                # fout.write("Depth(m)    rock_vel(Meter/second)\n")
                 split_list = self.descr.split(' ')
                 description = '_'.join(split_list)
                 fout.write("Depth(m)\t" + description + "(" + self.units + ")\n")
                 for d, v in zip(self.depth, self.data):
                     d = str(d)
-                    v = "1e30" if np.isnan(v) else str(v)
+                    v = str(v) if np.isfinite(v) else "1e30"
                     fout.write("\t".join([d, v]) + "\n")
         except Exception as inst:
             print(inst.args)
