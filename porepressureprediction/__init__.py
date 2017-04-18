c10b7204102b9c0ad29a6090df67018933e10880
Yu Hao <yuhao89@live.cn>
2017-04-18 15:12:55 +0800

    edit __init__ for new structure

--- porepressureprediction/__init__.py
+++ porepressureprediction/__init__.py
@@ -1,15 +1,14 @@
-# from porepressureprediction.velocity.basic.laSQL import Well
-from porepressureprediction.velocity.basic.laSQL import Log
-from porepressureprediction.velocity.basic.laSQL import smooth_log, truncate_log, interpolate_log, shale
-from porepressureprediction.velocity.basic.seiSQL import SeisCube
-from porepressureprediction.velocity.basic.survey import Survey
-from porepressureprediction.velocity.reader import Reader
+# from porepressureprediction.basic.laSQL import Well
+from porepressureprediction.basic.well import Well
+from porepressureprediction.basic.laSQL import Log
+from porepressureprediction.basic.laSQL import smooth_log, truncate_log, interpolate_log, shale
+from porepressureprediction.basic.seiSQL import SeisCube
+from porepressureprediction.basic.survey import Survey
+from porepressureprediction.basic.reader import Reader
 from porepressureprediction.velocity.smoothing import smooth, smooth_2d
-from porepressureprediction.stochastic.krig import Krig
 from porepressureprediction.velocity.conversion import rms2int, int2rms, int2avg, avg2int, twt2depth
 from porepressureprediction.velocity.interpolation import interp_DW, spline_1d
 from porepressureprediction.velocity.extrapolate import set_v0, normal, slotnick, normal_dt
 from porepressureprediction.pressure.obp import overburden_pressure, gardner, traugott
 from porepressureprediction.pressure.pore_pressure import virgin_curve, unloading_curve, bowers, eaton
 from porepressureprediction.pressure.hydrostatic import hydrostatic_pressure
-from porepressureprediction.velocity.basic.well import Well
