b296d468b06ecfa01c79821f9b0fd9f85552dd9e
Yu Hao <yuhao89@live.cn>
2017-06-15 16:59:54 +0800

    add testing files

---
+++ test/test_func.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Test
+
+Created on Jun. 15th 2017
+"""
+__author__ = "yuhao"
+
+import unittest
+
+from .context import porepressureprediction
+
+
+class WellTestSuite(unittest.TestCase):
+    """Basic test cases."""
+
+    def test_void_log(self):
+        "test on boolean value of Log object"
+        log = porepressureprediction.Log()
+        self.assertFalse(log)
+
+if __name__ == '__main__':
+    unittest.main()
