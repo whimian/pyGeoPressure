c66f3adc1889b1e47b82593530252e34f26eceec
Yu Hao <yuhao89@live.cn>
2017-11-01 15:26:58 +0800

    add test for utils

---
+++ test/test_utils.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Test
+
+Created on Jun. 15th 2017
+"""
+__author__ = "yuhao"
+
+import unittest
+import numpy as np
+
+from .context import porepressureprediction as ppp
+
+
+class UtilsTestSuite(unittest.TestCase):
+    """
+    Test Case for utils.py
+    """
+    def test_rmse(self):
+        "test on rmse"
+        a = np.arange(0, 10, 0.05)
+        b = np.arange(0, 10, 0.05)
+        self.assertEqual(ppp.nmse(a, b), 0)
+
+if __name__ == '__main__':
+    unittest.main()
