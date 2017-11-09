e8c2cc0f1bf013be0cd63cf4d45c7f554e4734c9
Yu Hao <yuhao89@live.cn>
2017-11-09 23:14:34 +0800

    add test for hydrostatic.py

---
+++ test/test_hydrostatic.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Nov. 9th 2017
+"""
+import pytest
+import numpy as np
+import pygeopressure as ppp
+
+@pytest.fixture
+def depth():
+    return np.arange(10)
+
+def test_hydrostatic(depth):
+    assert (ppp.hydrostatic_pressure(depth) == \
+        np.array([0., 0.00980665, 0.0196133, 0.02941995, 0.0392266,
+                  0.04903325, 0.0588399, 0.06864655, 0.0784532,
+                  0.08825985])).all()
+    assert (np.round(ppp.hydrostatic_trace(depth), 6) == \
+        np.array([0., 0.009898, 0.019796, 0.029694, 0.039592, 0.04949,
+                  0.059388, 0.069286, 0.079184, 0.089082])).all()
