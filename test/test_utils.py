97ce2c4c6c5d1bf51a9206259002d84037a3808d
Yu Hao <yuhao89@live.cn>
2017-11-16 22:52:38 +0800

    add test for utils.py

--- test/test_utils.py
+++ test/test_utils.py
@@ -6,21 +6,25 @@ Created on Jun. 15th 2017
 """
 __author__ = "yuhao"
 
-import unittest
+import pytest
 import numpy as np
+import pygeopressure as ppp
 
-from .context import pygeopressure as ppp
+@pytest.fixture()
+def array_for_test():
+    return np.arange(0, 10, 0.05)
+
+def test__rmse(array_for_test):
+    assert ppp.rmse(array_for_test, array_for_test) == 0
 
 
-class UtilsTestSuite(unittest.TestCase):
-    """
-    Test Case for utils.py
-    """
-    def test_rmse(self):
-        "test on rmse"
-        a = np.arange(0, 10, 0.05)
-        b = np.arange(0, 10, 0.05)
-        self.assertEqual(ppp.nmse(a, b), 0)
+def test__nmse(array_for_test):
+    assert ppp.nmse(array_for_test, array_for_test) == 0
 
-if __name__ == '__main__':
-    unittest.main()
+
+def test__split_sequence():
+    sequence = [1] * 9
+    length = 2
+    output = [[1, 1]] * 4 + [[1]]
+    assert list(ppp.basic.utils.split_sequence(sequence, length)) ==\
+        output
