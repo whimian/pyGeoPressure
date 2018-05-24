11b34452b5148d03d9f85ffde8217aa01f2c2213
Yu Hao <yuhao89@live.cn>
2018-05-24 17:21:21 +0800

    restruct __init__

--- pygeopressure/__init__.py
+++ pygeopressure/__init__.py
@@ -1,7 +1,10 @@
 # from pygeopressure.basic.laSQL import Well
 from pygeopressure.basic.well import Well
+from pygeopressure.basic.well_storage import Well_Storage
 from pygeopressure.basic.well_log import Log
-from pygeopressure.basic.well_log import smooth_log, truncate_log, interpolate_log, shale, upscale_log, local_average
+from pygeopressure.basic.well_log import (
+    smooth_log, truncate_log, interpolate_log, shale,
+    upscale_log, local_average)
 from pygeopressure.basic.seiSQL import SeisCube
 from pygeopressure.basic.seiSEGY import SeiSEGY
 from pygeopressure.basic.survey import Survey
@@ -9,18 +12,24 @@ from pygeopressure.basic.reader import Reader
 from pygeopressure.basic.survey_setting import SurveySetting
 from pygeopressure.basic.threepoints import ThreePoints
 from pygeopressure.velocity.smoothing import smooth, smooth_2d, smooth_trace
-from pygeopressure.velocity.conversion import rms2int, int2rms, int2avg, avg2int, twt2depth
+from pygeopressure.velocity.conversion import (rms2int, int2rms, int2avg,
+                                               avg2int, twt2depth)
 from pygeopressure.velocity.interpolation import interp_DW, spline_1d
-from pygeopressure.velocity.extrapolate import set_v0, normal, slotnick, normal_dt
-from pygeopressure.pressure.obp import overburden_pressure, gardner, traugott, obp_trace
-from pygeopressure.pressure.pore_pressure import virgin_curve, \
-    invert_virgin, unloading_curve, invert_unloading, bowers, eaton, \
-    multivariate_virgin, invert_multivariate_virgin, multivariate_unloading, \
-    invert_multivariate_unloading, effective_stress_multivariate, \
-    pressure_multivariate, bowers_varu, pressure_multivariate_varu
-from pygeopressure.pressure.hydrostatic import hydrostatic_pressure, hydrostatic_trace
+from pygeopressure.velocity.extrapolate import (set_v0, normal, slotnick,
+                                                normal_dt)
+from pygeopressure.pressure.obp import (overburden_pressure, gardner, traugott,
+                                        obp_trace)
+from pygeopressure.pressure.pore_pressure import (
+    virgin_curve, invert_virgin, unloading_curve, invert_unloading,
+    bowers, eaton, multivariate_virgin, invert_multivariate_virgin,
+    multivariate_unloading, invert_multivariate_unloading,
+    effective_stress_multivariate, pressure_multivariate, bowers_varu,
+    pressure_multivariate_varu)
+from pygeopressure.pressure.hydrostatic import (hydrostatic_pressure,
+                                                hydrostatic_trace)
 from pygeopressure.basic.utils import rmse, nmse
-from pygeopressure.basic.indexes import InlineIndex, CrlineIndex, DepthIndex, CdpIndex
+from pygeopressure.basic.indexes import (InlineIndex, CrlineIndex, DepthIndex,
+                                         CdpIndex)
 from pygeopressure.basic.horizon import Horizon
 
 from ._version import get_versions
