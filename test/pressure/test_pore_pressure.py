9da61401dd4ee1689636b461ad09e13d69c21cc0
Yu Hao <yuhao89@live.cn>
2018-08-23 16:57:36 +0800

    add test for velocity

--- test/pressure/test_pore_pressure.py
+++ test/pressure/test_pore_pressure.py
@@ -16,20 +16,27 @@ def test__unloading_curve():
     assert ppp.unloading_curve(36, 98, 0.5, 1, 4000) == 2112
     assert ppp.invert_unloading(2112, 98, 0.5, 1, 4000) == 36
 
+
 @pytest.fixture()
 def obp():
-    return np.full((3,), fill_value=100, dtype=np.float32)
+    return np.full((10,), fill_value=100, dtype=np.float32)
+
 
 @pytest.fixture()
 def vel():
-    return np.full((3,), fill_value=2112, dtype=np.float32)
+    return np.full((10,), fill_value=2112, dtype=np.float32)
+
 
 def test__bowers(vel, obp):
     assert (ppp.bowers(vel, obp, 1, 1, 98, 0.5, 4000) == \
-        np.full((3,), fill_value=64, dtype=np.float32)).all()
-    # u = np.array([1,2,3])
-    # assert (ppp.bowers_varu(vel, obp, u, 0, 98, 0.5, 4000, buffer=1) == \
-    #     np.array([64, 64, 64])).all()
+        np.full((10,), fill_value=64, dtype=np.float32)).all()
+
+def test__bowers_varu(vel, obp):
+    assert (np.array(
+        ppp.bowers_varu(vel, obp, 3, 3, 98, 0.5, 4000,
+                        buf=2, end_idx=6, end_buffer=2), dtype=np.float32) == \
+        np.array([64]*3 + [99.88549805]*4 + [64]*3, dtype=np.float32)).all()
+
 
 def test__eaton():
     assert ppp.eaton(2112, 2112, 500, 1000) == 500
---
+++ test/velocity/test_conversion.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Aug. 23rd 2018
+"""
+import pytest
+import numpy as np
+import pygeopressure as ppp
+
+
+@pytest.fixture()
+def twt():
+    return np.array([1000, 2000, 3000, 4000])
+
+
+@pytest.fixture()
+def vel():
+    return np.array([1500, 1800, 2200, 2100])
+
+
+def test__rms_int(twt, vel):
+    assert (vel == ppp.rms2int(twt, ppp.int2rms(twt, vel))).all()
+
+
+def test__avg_int(twt, vel):
+    assert (vel == ppp.avg2int(twt, ppp.int2avg(twt, vel))).all()
+
+
+def test_time2depth(twt, vel):
+    _, new_vel = ppp.twt2depth(twt, vel, vel, stepDepth=500)
+    assert new_vel[0] == vel[0]
