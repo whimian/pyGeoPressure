33b656c7ef45c007aa41402c0a13e3b404225214
Yu Hao <yuhao89@live.cn>
2018-09-21 09:20:21 +0800

    use predefined function

--- pygeopressure/basic/optimizer.py
+++ pygeopressure/basic/optimizer.py
@@ -23,6 +23,7 @@ from pygeopressure.pressure.obp import traugott
 from pygeopressure.basic.well import Well
 from pygeopressure.basic.well_log import Log
 from pygeopressure.basic.utils import rmse
+from pygeopressure.pressure.eaton import ratio_eaton
 
 
 def pick_sparse(a_array, n):
@@ -199,9 +200,8 @@ def optimize_eaton(well, vel_log, obp_log, a, b, pres_log="loading"):
     es = obp - pres
     es_norm = np.array(es_norm)
     es_ratio = es / es_norm
-    def func(x, n):
-        return x**n
-    popt, _ = curve_fit(func, vel_ratio, es_ratio)
+
+    popt, _ = curve_fit(ratio_eaton, vel_ratio, es_ratio)
     n, = popt
 
     return n
