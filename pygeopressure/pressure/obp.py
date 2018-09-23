a69c1b68806ff43d5e7320ab7a26b349050dbcb2
Yu Hao <yuhao89@live.cn>
2018-09-23 17:11:42 +0800

    add utils for pressure

--- pygeopressure/pressure/obp.py
+++ pygeopressure/pressure/obp.py
@@ -6,13 +6,11 @@ from __future__ import division, print_function, absolute_import
 
 __author__ = "yuhao"
 
-import json
 from collections import OrderedDict
 import numpy as np
 from pygeopressure.basic.well_log import Log
-from pygeopressure.basic.seisegy import SeiSEGY
 from pygeopressure.basic.indexes import InlineIndex
-from . import Path
+from pygeopressure.pressure.utils import create_seis, create_seis_info
 
 
 def traugott(z, a, b):
@@ -81,32 +79,6 @@ def gardner(v, c=0.31, d=0.25):
     return c * v**d
 
 
-def create_seis(name, like):
-    # create output segy file
-    input_path = Path(like.segy_file)
-    output_path = input_path.parent / "{}.sgy".format(name)
-    return SeiSEGY(str(output_path), like=str(like.segy_file))
-
-def create_seis_info(segy_object, name):
-    file_path = Path(segy_object.segy_file).absolute()
-    parent_folder = file_path.parent
-    dict_info = OrderedDict([
-        ("path", str(file_path)),
-        ("inDepth", segy_object.inDepth),
-        ("Property_Type", segy_object.property_type),
-        ("inline_range", [segy_object.startInline,
-                          segy_object.endInline,
-                          segy_object.stepInline]),
-        ("crline_range", [segy_object.startCrline,
-                          segy_object.endCrline,
-                          segy_object.stepCrline]),
-        ("z_range", [segy_object.startDepth,
-                     segy_object.endDepth,
-                     segy_object.stepDepth])])
-    with open(str(parent_folder / ".{}".format(name)), 'w') as fl:
-        json.dump(dict_info, fl, indent=4)
-
-
 def gardner_seis(output_name, vel_cube, c=0.31, d=0.25):
     """
     Parameters
@@ -118,11 +90,6 @@ def gardner_seis(output_name, vel_cube, c=0.31, d=0.25):
     -------
     SeiSEGY
     """
-    # create output cube
-    # input_path = Path(vel_cube.segy_file)
-    # output_path = input_path.parent / output_file
-    # den_cube = SeiSEGY(str(output_path), like=str(vel_cube.segy_file))
-
     den_cube = create_seis(output_name, vel_cube)
     create_seis_info(den_cube, output_name)
     # calculate density
---
+++ pygeopressure/pressure/utils.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+some utilities regarding pressure calculation
+"""
+from __future__ import division, print_function, absolute_import
+
+__author__ = "yuhao"
+
+import json
+from collections import OrderedDict
+
+from pygeopressure.basic.seisegy import SeiSEGY
+from . import Path
+
+
+def create_seis(name, like):
+    # create output segy file
+    input_path = Path(like.segy_file)
+    output_path = input_path.parent / "{}.sgy".format(name)
+    return SeiSEGY(str(output_path), like=str(like.segy_file))
+
+
+def create_seis_info(segy_object, name):
+    file_path = Path(segy_object.segy_file).absolute()
+    parent_folder = file_path.parent
+    dict_info = OrderedDict([
+        ("path", str(file_path)),
+        ("inDepth", segy_object.inDepth),
+        ("Property_Type", segy_object.property_type),
+        ("inline_range", [segy_object.startInline,
+                          segy_object.endInline,
+                          segy_object.stepInline]),
+        ("crline_range", [segy_object.startCrline,
+                          segy_object.endCrline,
+                          segy_object.stepCrline]),
+        ("z_range", [segy_object.startDepth,
+                     segy_object.endDepth,
+                     segy_object.stepDepth])])
+    with open(str(parent_folder / ".{}".format(name)), 'w') as fl:
+        json.dump(dict_info, fl, indent=4)
