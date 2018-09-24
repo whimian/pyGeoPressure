8f8c88b0143a8e7fb842eaf3790d50bb084f44fe
Yu Hao <yuhao89@live.cn>
2018-09-24 19:27:19 +0800

    remove __main__ part

--- pygeopressure/basic/las_reader.py
+++ pygeopressure/basic/las_reader.py
@@ -378,27 +378,3 @@ class LASReader(object):
 
         if opened_here:
             f.close()
-
-
-if __name__ == "__main__":
-    import sys
-
-    las = LASReader(sys.argv[1], null_subs=np.nan)
-    print("wrap? ", las.wrap)
-    print("vers? ", las.vers)
-    print("null =", las.null)
-    print("start =", las.start)
-    print("stop  =", las.stop)
-    print("step  =", las.step)
-    print("Version ---")
-    las.version.display()
-    print("Well ---")
-    las.well.display()
-    print("Curves ---")
-    las.curves.display()
-    print("Parameters ---")
-    las.parameters.display()
-    print("Other ---")
-    print(las.other)
-    print("Data ---")
-    print(las.data2d)
