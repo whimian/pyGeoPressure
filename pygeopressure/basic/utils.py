7bb7cf5227d05130b524f92068404618b7895c66
Yu Hao <yuhao89@live.cn>
2019-02-16 12:09:56 +0800

    add gslib file conversion

--- pygeopressure/basic/utils.py
+++ pygeopressure/basic/utils.py
@@ -12,6 +12,7 @@ __author__ = "yuhao"
 import sys
 
 import numpy as np
+import pandas as pd
 
 PY_VER = sys.version_info.major
 
@@ -84,3 +85,45 @@ def pick_sparse(a_array, n):
         return np.append(new_array, [a_array[-1]])
     else:
         return new_array
+
+
+def gslib_to_od(gslib_file, od_file):
+    """
+    Convert gslib file to opendtect file
+    """
+    with open(gslib_file, 'r') as fl:
+        info = fl.readline().strip('\n')
+        header_len = int(fl.readline().strip('\n'))
+        column_name = [fl.readline().strip('\n') for i in range(header_len)]
+    df = pd.read_csv(
+        gslib_file, sep=str(' '), header=None, names=column_name,
+        index_col=False, skiprows=header_len+2)
+    df.groupby(['X', 'Y'])[column_name[-1]].apply(list).to_csv(
+        od_file, sep='\t')
+
+
+def merge_gslib(gslib_files, output_file):
+    df_list = []
+    property_name = []
+    for gslib_f in gslib_files:
+        with open(gslib_f, 'r') as fl:
+            info = fl.readline().strip('\n')
+            header_len = int(fl.readline().strip('\n'))
+            column_name = [fl.readline().strip('\n') for i in range(header_len)]
+            property_name.append(column_name[-1])
+
+        df_list.append(pd.read_csv(
+            gslib_f, sep=str(' '), header=None, names=column_name,
+            index_col=False, skiprows=header_len+2))
+    new_df = df_list[0]
+    for df in df_list[1:]:
+        new_df  = pd.merge(new_df, df, how='left', on = ['x', 'y', 'z'])
+    with open(output_file, 'w') as out_fl:
+        out_fl.write("Merged by pyGeoPressure\n")
+        out_fl.write("{}\n".format(len(property_name)+3))
+        out_fl.write("x\n")
+        out_fl.write("y\n")
+        out_fl.write("z\n")
+        for prop in property_name:
+            out_fl.write("{}\n".format(prop))
+    new_df.to_csv(output_file, sep='\t', mode='a', header=False, index=False)
