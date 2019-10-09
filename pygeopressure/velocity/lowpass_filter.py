3ceec386eb3b90b274b292ec2500eb7dab654b51
Yu Hao <yuhao89@live.cn>
2019-10-09 15:41:03 +0800

    add lowpass_filter

---
+++ pygeopressure/velocity/lowpass_filter.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Low pass filter
+
+Created on Oct 9
+"""
+import numpy as np
+from scipy import signal
+from scipy.interpolate import interp1d
+
+
+def butter_lowpass(cutoff, nyq_freq, order=4):
+    normal_cutoff = float(cutoff) / nyq_freq
+    b, a = signal.butter(order, normal_cutoff, btype='lowpass')
+    return b, a
+
+
+def butter_lowpass_filter(data, cutoff_freq, nyq_freq, order=4):
+    # Source: https://github.com/guillaume-chevalier/filtering-stft-and-laplace-transform
+    b, a = butter_lowpass(cutoff_freq, nyq_freq, order=order)
+    y = signal.filtfilt(b, a, data)
+    return y
